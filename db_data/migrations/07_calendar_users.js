e5461f67e921bbf7451b5d4bb11cfcfae6c84d01
fudpucker1 <fudpucker.hammy@protonmail.com>
2024-04-19 15:02:14 -0400

    fixed migration/seed issue

--- db_data/migrations/07_calendar_users.js
+++ db_data/migrations/07_calendar_users.js
@@ -7,9 +7,10 @@ exports.up = function(knex) {
     table.string('username');
     table.string('password');
     table.integer('user_type').unsigned();
-    table.foreign('user_type').references('user_types.user_type_id');
+    table.foreign('user_type').references('user_type.user_type_id');
     table.integer('team_id').unsigned();
     table.foreign('team_id').references('calendar_teams.team_id');
+    table.integer('supervisor_id').unsigned();
   });
 };
 
