1dad59624ed4527bee5d59400b003762ef7bb59e
TardigradeTyrs <morrison.kendrick7@gmail.com>
2024-04-19 13:13:27 -0600

    Base React App

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from './Features/Home'
+import { Login } from './Features/Login'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome!</h1>
+      <nav>
+    <div className = 'navbarcontainer'>
+        <ul className = 'navbar' style={{ listStyleType: "none" }} >
+          <li style={{marginRight: '10px'}} ><Link to="/">Home</Link></li>
+          <li style={{marginRight: '10px' }} ><Link to="/Login">Login</Link></li>
+          <li style={{marginRight: '10px' }} ><Link to="/mycalendar">My Calendar</Link></li>
+
+      </ul>
+      </div>
+    </nav>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+
+    </Routes>
+  </div>
+  );
+}
+
+export default App;
