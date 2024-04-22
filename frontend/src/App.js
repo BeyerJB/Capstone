3889997089880559256e01dabbeb348879549a03
TardigradeTyrs <morrison.kendrick7@gmail.com>
2024-04-22 09:36:04 -0600

    Login Feature and cookies working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { useContext } from "react"
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { Home } from './Features/Home'
-import { Login } from './Features/Login'
+import Login from './Features/Login'
+import { AuthContext } from './Features/AuthContext';
+import {useCookies} from 'react-cookie'
 
 
 function App() {
+  const { logout } = useContext(AuthContext)
+  const [cookies, setCookie, removeCookie] = useCookies(['userID', 'firstName', 'lastName', 'rank']);
+  const navigate = useNavigate()
   return (
+    <>
     <div className="App">
       <h1>Welcome!</h1>
       <nav>
     <div className = 'navbarcontainer'>
         <ul className = 'navbar' style={{ listStyleType: "none" }} >
           <li style={{marginRight: '10px'}} ><Link to="/">Home</Link></li>
+          {!cookies.userID ?
           <li style={{marginRight: '10px' }} ><Link to="/Login">Login</Link></li>
+          : [] }
           <li style={{marginRight: '10px' }} ><Link to="/mycalendar">My Calendar</Link></li>
 
       </ul>
@@ -21,9 +30,17 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
-
+      <Route path='/mycalendar' element={<p>Calendar</p>} />
     </Routes>
   </div>
+  <footer>
+    {cookies.userID ?
+    <button onClick={() => logout()} >Log out</button>
+  :
+  []
+  }
+  </footer>
+  </>
   );
 }
 
--- frontend/src/Features/Login.js
+++ frontend/src/Features/Login.js
@@ -1,5 +1,51 @@
+import React, { useState, useContext } from 'react';
+import { AuthContext } from './AuthContext';
+import { useNavigate } from "react-router-dom";
 
+function Login() {
+  const [loginData, setLoginData] = useState({username: '', password: ''})
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-export const Login = () => {
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setLoginData(prevItem => ({
+      ...prevItem,
+      [name]: value
+    }));
+  };
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    fetch("http://localhost:8080/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(loginData)
+    })
+    .then(async (res) => {
+      if (res.status === 200) {
+        const response = await res.json();
+        login(response);
+
+        navigate('/mycalendar');
+      } else {
+        const errorMessage = await res.text();
+        alert(`${errorMessage}`);
+      }
+    });
+  };
+
+  return (
+    <div className="log-form">
+      <h2>Login to your account</h2>
+      <form onSubmit={handleLogin}>
+        <input type="text" placeholder="username" name="username" value={loginData.username} onChange={handleInputChange}/>
+        <input type="password" placeholder="password" name="password" value={loginData.password} onChange={handleInputChange}/>
+        <button type="submit" className="btn">Login</button>
+      </form>
+    </div>
+  );
 }
+
+export default Login;
