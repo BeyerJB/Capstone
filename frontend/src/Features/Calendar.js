d1db79890ce4f4dd249d15645622cd8d10a889b9
TuanTrinh <tuan.a.trinh@gmail.com>
2024-04-23 11:19:26 -1000

    finished. Awaiting edit features to be developed to add functions to buttons in modal

--- frontend/src/Features/Calendar.js
+++ frontend/src/Features/Calendar.js
@@ -5,13 +5,19 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import multiMonthPlugin from "@fullcalendar/multimonth";
 import interactionPlugin from "@fullcalendar/interaction";
-import "../App.css";
+import Modal from 'react-modal'
+// import Popup from 'reactjs-popup';
+// import "../App.css";
 
 export const Calendar = () => {
   const [cookies] = useCookies(['userID', 'firstName', 'lastName', 'rank']);
   const [events, setEvents] = useState([]);
   const [allData, setAllData] = useState([]);
   const userId = cookies.userID;
+  //Modal stuff
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
   // const [newEvents, setNewEvents] = useState([]);
   // const [title, setTitle] = useState('');
   // const [startDate, setStartDate] = useState('');
@@ -29,12 +35,25 @@ export const Calendar = () => {
           title: event.title,
           start: event.start_datetime,
           end: event.end_datetime,
+          description: event.description
         }));
         setEvents(formattedEvents);
       })
       .catch(error => console.error('Error fetching events: ', error));
   }, [userId]);
 
+  //handle open modal
+  const openModal = (event) => {
+    setSelectedEvent(event);
+    setIsModalOpen(true);
+  };
+
+  //handle close modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  console.log(selectedEvent)
   // const handleSubmit = (e) => {
   //   e.preventDefault();
   //   const eventData = {
@@ -68,11 +87,30 @@ export const Calendar = () => {
             center: 'title',
             end: 'timeGridDay,timeGridWeek,dayGridMonth,multiMonthYear'
           }}
+
+          eventClick={openModal}
+
+          nowIndicator='true'
+          dayMaxEvents='true'
+          selectable='true'
           handleWindowResize='true'
           aspectRatio='2'
           multiMonthMaxColumns='12'
           events={events}
         />
+      </div>
+      <div style={{ position: 'absolute', visibility: 'hidden', zIndex: 12001, width: '158px', padding: '2px 0 0 0' }}>
+      <Modal isOpen={isModalOpen} onRequestClose={closeModal} style={{ overlay: { backgroundColor: 'rgba(255, 255, 255, .9)', zIndex: 9999 }, content: { backgroundColor: 'white', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', overflow: 'auto', maxHeight: '80vh', maxWidth: '80vw', zIndex: 10000, padding: '15px'} }}>
+        <h2>Event: {selectedEvent ? selectedEvent.event.title : ''}</h2>
+        <p>Start: {selectedEvent ? selectedEvent.event.start.toString() : ''}</p>
+        <p>End: {selectedEvent ? selectedEvent.event.end.toString() : ''}</p>
+        <p>Description: {selectedEvent ? selectedEvent.event.extendedProps.description : ''}</p>
+        <div style={{ marginTop: '370px'}}>
+          <button style={{ marginRight: '20px' }}>Button 1</button>
+          <button style={{ marginRight: '20px' }}>Button 2</button>
+          <button style={{ float: 'right' }} onClick={closeModal}>Close</button>
+        </div>
+      </Modal>
       </div>
       {/* <div className={`sidebar ${isOpen ? 'open' : ''}`}>
         <h2>Add New Event</h2>
@@ -106,3 +144,8 @@ export const Calendar = () => {
     </>
   );
 };
+
+// eventClick={function(info){
+//   console.log('Test', info.event)
+//   alert('Event: ' + info.event.title + `\n` + 'Start: ' + info.event.start + `\n` + 'End: ' + info.event.end + `\n` + 'Details: ' + info.event.extendedProps.description)
+// }}
