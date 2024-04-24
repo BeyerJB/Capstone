5151be8d9ad35ada6ce26d673f83e3e540b541b5
TardigradeTyrs <morrison.kendrick7@gmail.com>
2024-04-24 13:09:09 -0600

    Added Modal view

--- frontend/src/Features/TeamView.js
+++ frontend/src/Features/TeamView.js
@@ -5,10 +5,19 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import { useCookies } from 'react-cookie'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 export const TeamView = () => {
   const [cookies] = useCookies(['userID', 'firstName', 'lastName', 'rank']);
   const [ teamEvents, setTeamEvents ] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
 
   useEffect(() => {
     console.log("cookie id", `http://localhost:8080/api/teamview/${cookies.userID}`)
@@ -17,7 +26,14 @@ export const TeamView = () => {
       .then(jsonRes => setTeamEvents(jsonRes))
   }, [cookies.userID])
 
-  console.log(teamEvents)
+  const openModal = (event) => {
+    setSelectedEvent(event);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="calendar">
@@ -72,6 +88,7 @@ export const TeamView = () => {
         aspectRatio={2}
         nowIndicator = {true}
         editable={false}
+        eventClick={openModal}
         resourceAreaHeaderContent="Guardians"
         resourceAreaWidth = "15vw"
         resourceGroupField= 'team_name'
@@ -99,6 +116,27 @@ export const TeamView = () => {
           })
       }
       />
+      <div style={{ position: 'absolute', visibility: 'hidden', zIndex: 12001, width: '158px', padding: '2px 0 0 0',  textDecoration: 'none' }}>
+      <Modal show={isModalOpen} onHide={closeModal} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Event: {selectedEvent ? selectedEvent.event.title : ''}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Start: {selectedEvent ? selectedEvent.event.start.toString() : ''}</p>
+          <p>End: {selectedEvent ? selectedEvent.event.end.toString() : ''}</p>
+          <p>Description: {selectedEvent ? selectedEvent.event.extendedProps.description : ''}</p>
+        </Modal.Body>
+        <Modal.Footer>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+          <div>
+            <Button variant="primary">Button 1</Button>
+            <Button variant="secondary" style={{ marginLeft: '10px' }}>Button 2</Button>
+          </div>
+          <Button variant="danger" onClick={closeModal}>Close</Button>
+        </div>
+        </Modal.Footer>
+      </Modal>
+      </div>
     </div>
   );
 };
